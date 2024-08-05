261e89482d85a965cf51849c55182822da49dd14
LeCloutPanda <53411604+LeCloutPanda@users.noreply.github.com>
2024-08-05 14:58:55 +1000

    More stuff

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const {randomUUID, createHash, randomBytes} = require("crypto");
 const signalR = require("@microsoft/signalr");
-const fetch = require('node-fetch');
 
 const API = "https://api.resonite.com/";
 const ASSET_URL = "https://assets.resonite.com/"
@@ -44,6 +43,7 @@ class ResoNetLib {
         }
 
         this.signalRConnection = undefined;
+        this.log("Intializing Client.");
     }
 
     async start() {
@@ -57,6 +57,10 @@ class ResoNetLib {
     }
 
     async login() {   
+        if (this.data.loggedIn) {
+            this.error("Already logged in!");
+        }
+
         const loginData = {
             "username": this.config.username,
             "authentication": {
@@ -88,27 +92,29 @@ class ResoNetLib {
             this.data.loggedIn = true;
         }
         else {
-            throw new Error(`Unexpected return code ${res.status}: ${await res.text()}`);
+            let response = await res.text();
+            //this.error(`Unexpected return code ${res.status}: ${response}`);
+            throw new Error(`Unexpected return code ${res.status}: ${response}`);
         }
     
-        console.log("Successfully Logged in");
+        this.log("Successfully Logged in");
     }
     
     async logout() {
-        if(!data.loggedIn){
-            throw new Error("Failed logging out, no user is logged in.");
+        if (!this.data.loggedIn) {
+            this.error("Not logged in! Can't log out.");
         }
     
-        const res = await fetch(`${API}/userSessions/${data.userId}/${data.token}`,
+        const res = await fetch(`${API}/userSessions/${this.data.userId}/${this.data.token}`,
         {
             method: "DELETE",
             headers: {
-                "Authorization": data.fullToken
+                "Authorization": this.data.fullToken
             }
-        }
-        );
+        });
     
         if (res.status !== 200){
+            //this.error(`Unexpected HTTP status when logging out (${res.status} ${res.statusText}): ${res.body}`);
             throw new Error(`Unexpected HTTP status when logging out (${res.status} ${res.statusText}): ${res.body}`);
         }
     
@@ -119,7 +125,7 @@ class ResoNetLib {
     }
     
     async startSignalR() {
-        signalRConnection = new signalR.HubConnectionBuilder()
+        this.signalRConnection = new signalR.HubConnectionBuilder()
         .withUrl(`${API}/hub`, {
             headers: {
                 "Authorization": this.data.fullToken,
@@ -151,14 +157,153 @@ class ResoNetLib {
         */
     
         this.signalRConnection.start();
-        console.log("Starting SignalR");
+        this.log("Starting SignalR");
     }
     
-    async stopSignalR(){
-        await signalRConnection.stop();
+    async stopSignalR() {
+        await this.signalRConnection.stop();
         this.signalRConnection = undefined;
-        console.log("Stopping SignalR.");
+        this.log("Stopping SignalR.");
+    }
+
+    // Create side function that automatically parses all avaliable varaibles into a string and return that instead of just raw json
+    async fetchUser(userid) {
+        let url = `${API}users/${userid}` + (userid.startsWith('U-') ? "" : "?byusername=true");
+        this.log(`Fetching user data for "${userid}"`);
+        const res = await fetch(url);
+
+        if (res.ok) {
+            const json = await res.json();
+            return json;
+        } else {
+            const text = await res.text();
+            return text;
+        }
+    }
+
+    async fetchUsers(query) {
+        var apiUrl = ""
+        if (query.startsWith("U-")) apiUrl = `${API}/users/${query}`;
+        else apiUrl = `${API}/users?name=${query}`;
+        
+        this.log(`Fetching users with name of "${query}"`);
+        const res = await fetch(apiUrl);
+
+        if (res.ok) {
+            const json = await res.json();
+            return json;
+        } else {
+            const text = await res.text();
+            return text;
+        }
+    }
+
+    async addFriend(user) {
+        if (!this.data.loggedIn) {
+            this.error("Not logged in! Can't add friend");
+        }
+
+        user = await this.fetchUser(user);
+
+        contact = {};
+        //contact.OwnerId = this.data.userId;
+        //contact.ContactUserId = user.id;
+        //contact.ContactUsername = user.username;
+        //contact.friendStatus = "Requested";
+    }
+
+    async removeFriend(userid) {
+        if (!this.data.loggedIn) {
+            this.error("Not logged in! Can't remove friend");
+        }
+
+        await fetch(`${baseAPIURL}/users/${this.data.userId}/friends/${userid}`,
+        {
+            method: "DELETE",
+            headers: {
+                "Authorization": this.data.fullToken
+            }
+        }).then(() => {
+            this.log(`Successfully removed "${userid}" as a contact.`);
+        }).catch(async (error) => {
+            this.error(error);
+        });
+    }
+
+    async blockuser(user) {
+        if (!this.data.loggedIn) {
+            this.error("Not logged in! Can't block friend");
+        }
+    }
+
+    async sendRawMessage(messageData){
+        if (!this.data.loggedIn) {
+            this.error("Not logged in! Can't send raw message.");
+        }
+
+        await this.signalRConnection.send("SendMessage", messageData).catch(async (error) => {
+            this.error(error);
+        });
+    }
+
+    async sendTextMessage(userid, content) {
+        if (!this.data.loggedIn) {
+            this.error("Not logged in! Can't send message.");
+        }
+
+        if (!userid.startsWith('U-')) {
+            this.error("UserId is not a user id.")
+            return;
+        } 
+        else if (content.trim() == "") {
+            this.error("Content is null");
+            return;
+        }
+
+        const messageData = {
+            "id": `MSG-${randomUUID()}`,
+            "senderId": this.data.userId,
+            "recipientId": userid,
+            "messageType": "Text",
+            "sendTime": (new Date(Date.now())).toISOString(),
+            "lastUpdateTime": (new Date(Date.now())).toISOString(),
+            "content": content
+        }
+
+        await this.signalRConnection.send("SendMessage", messageData).catch(async (error) => {
+            this.error(error);
+        });
+    }
+
+    async fetchContacts() {
+        if (!this.data.loggedIn) {
+            this.error("Not logged in! Can't fetch contacts.");
+        }
+
+        const res = await fetch(`${API}/users/${this.data.userId}/contacts`, {headers: {"Authorization": this.data.fullToken}});
+        let json = await res.json();      
+        return json;
+    }
+
+    log(message) {
+        console.log(`[${Date.now()} INFO] ${message}`);
+    }
+    
+    warning(message) {
+        console.warn(`[${Date.now()} WARN] ${message}`);
+    }
+    
+    error(message) {
+        console.error(`[${Date.now()} ERROR] ${message}`);
+    }
+
+    formatIconUrl(url) {
+        try {
+            return url.replace('resdb://', ASSET_URL).replace('.webp', '').replace('.png', '');
+        }
+        catch {
+            return 'INVALID_URL';
+        }
     }
 }
-
 module.exports = ResoNetLib;
