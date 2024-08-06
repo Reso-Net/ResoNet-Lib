007d9dba7ac9a0dbd21be69d0e3b3e340c09cb47
Sveken <sveken@live.com.au>
2024-08-07 08:30:35 +1000

    Proof Read adjustments

--- examples/fetch_contacts.js
+++ examples/fetch_contacts.js
@@ -3,7 +3,7 @@ const ResoNetLib = require('resonet-lib');
 // Load the config to be used for the client
 const config = require('../config.json');
 
-// This is to instantiate an instance of the ResoNetLib client
+// This is to initiate an instance of the ResoNetLib client
 const client = new ResoNetLib(config);
 
 async function setup() {
@@ -15,7 +15,7 @@ async function setup() {
     const contacts = await client.fetchContacts();
     // We output the count of contacts found on the logged in account
     client.log(`Found ${contacts.length} of ${client.data.userId}`);
-    // We now iterate over every item in the contacts and ouput the contact neatly formatted to increase readibility
+    // We now iterate over every item in the contacts and output the contact neatly formatted to increase readability
     contacts.forEach(contact => {
         client.log(`Username ${contact.contactUsername}, Contact Status ${contact.contactStatus}, Is Accepted: ${contact.isAccepted}`);
     })
